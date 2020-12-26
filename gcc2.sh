37d9a94d0b098fd3464ee869369cfcfbf293cbf4
EC2 Default User <ec2-user@ip-172-30-1-36.ap-northeast-1.compute.internal>
2020-12-26 04:30:01 +0000

    support echo

--- gcc2.sh
+++ gcc2.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-echo "1"
-rm -f a.out;echo -e "#include <stdio.h>\x0Aint main(int argc,char *argv[]){};" | gcc -x c -;./a.out
-echo '2'
-rm -f a.out;echo -e "#include <stdio.h>\x0Aint main(int argc,char *argv[]){};" | gcc -m32 -x c -;./a.out
-echo 'OK'
+echo "echo '"
+echo -e "#include <stdio.h>\x0Aint main(int argc,char *argv[]){};" | gcc -x c -;./a.out
+echo -e "#include <stdio.h>\x0Aint main(int argc,char *argv[]){};" | gcc -m32 -x c -;./a.out
+echo "'"
 
 
 
